144a35d639ccd3a60f100793df00fd62aa81766b
TomV <tom@viner.tv>
2016-04-16 00:52:58 +0100

    document no trust algo

--- game.py
+++ game.py
@@ -1,9 +1,33 @@
 """
-For player in game:
+Play with trust:
+
+for player in game:
     if current player:
         send move
     else:
         listen for move
         receive move
+decide winner
+
+
+Play trusting no one:
+
+Swap hashes:
+    for player in game:
+        if current player:
+            send hasher(move + salt)
+        else:
+            listen for hash
+            receive hash
+
+Swap salts:
+    for player in game:
+        if current player:
+            send move + salt
+        else:
+            listen for move + salt
+            receive move + salt
+            verify hasher(move + salt) == hash
+
 decide winner
 """
