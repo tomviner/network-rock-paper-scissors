a850b250c61d1552093fa2ef2843d5eafbc3056e
TomV <tom@viner.tv>
2016-04-17 22:44:53 +0100

    add waits with timeout

--- test_game.py
+++ test_game.py
@@ -18,6 +18,8 @@ def test_game(p1_mv, p2_mv, res):
     pool = Pool(2, maxtasksperchild=1)
     res1 = pool.apply_async(play, args=(1, p1_mv))
     res2 = pool.apply_async(play, args=(2, p2_mv))
+    res1.wait(timeout=5)
+    res2.wait(timeout=5)
     pool.close()
     pool.join()
     assert res1.successful()
