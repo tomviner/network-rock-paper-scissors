48b588cf0bcff2650e0581f57daa22b25aa05b23
TomV <tom@viner.tv>
2016-05-08 21:48:39 +0100

    subproc: communicate should block

--- tests/test_game_subprocess.py
+++ tests/test_game_subprocess.py
@@ -10,14 +10,7 @@ def test_game(turn, beacon):
         [sys.executable, 'netrps/game.py', '2', turn.p2_mv],
         stdout=PIPE)
 
-    while (
-        proc1.returncode is None and
-        proc2.returncode is None
-    ):
-        proc1.poll()
-        proc2.poll()
-
     for proc in (proc1, proc2):
-        assert proc.returncode == 0
         output = str(proc.communicate()[0])
+        assert proc.returncode == 0
         assert str(turn.expected_res) in output
